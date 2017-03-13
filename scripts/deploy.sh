fd6c29819c0406bd37b5f803cd28e29e5c9ed1ce
delichty <delichty@panologyinc.com>
2017-03-13 15:46:29 -0500

    made deploy script portable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 # deploy.sh
 
+#!! Note !!
+# Make sure your shell enviornment has the following configured
+#   AWS_DEFAULT_PROFILE - The CLI profile to use
+#   AWS_ACCOUNT_ID - The (numeric) account ID to use
+if [ -z "${AWS_ACCOUNT_ID}" -o -z "${AWS_DEFAULT_PROFILE}" ]; then
+  echo "Both AWS_ACCOUNT_ID and AWS_DEFAULT_PROFILE must be defined in your shell."
+  echo -e "Aborting.\n"
+  exit 1
+fi
+
 # Configuration
-AWS_PROFILE="delichty"
-BUCKET="ourpts-common-ohio-454844014023"
+#AWS_PROFILE="delichty"
+BUCKET="ourpts-common-ohio-${AWS_ACCOUNT_ID}"
 S3PATH="lambda"
 PACKAGE="aws-cloudformation-cloudfront-identity.zip"
 
@@ -11,4 +21,4 @@ echo "Biulding the package"
 #node_modules/.bin/cfn-lambda zip --output deploy/archive.zip
 
 echo "Deploy version to S3"
-aws --profile ${AWS_PROFILE} s3 cp deploy/archive.zip s3://${BUCKET}/${S3PATH}/${PACKAGE}
+aws s3 cp deploy/archive.zip s3://${BUCKET}/${S3PATH}/${PACKAGE}
