b7cf5c4f3cf978c4fe16937313cda5c9fac79bb9
delichty <delichty@panologyinc.com>
2017-06-30 08:52:20 -0500

    Only update Lambda Function if it exists

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,9 +16,22 @@ fi
 BUCKET="ourpts-common-ohio-${AWS_ACCOUNT_ID}"
 S3PATH="lambda"
 PACKAGE="aws-cloudformation-cloudfront-identity.zip"
+LAMBDA="ourpts-cfn-cloudfront-identity"
 
-echo "Biulding the package"
-#node_modules/.bin/cfn-lambda zip --output deploy/archive.zip
+echo "Building the package"
+node_modules/.bin/cfn-lambda zip --output deploy/archive.zip
 
-echo "Deploy version to S3"
+echo "Deploying version to S3"
 aws s3 cp deploy/archive.zip s3://${BUCKET}/${S3PATH}/${PACKAGE}
+
+echo "Updating Lambda function"
+LAMBDA=$(aws lambda list-functions \
+  | jq '.Functions[] | select(.FunctionName == "ourpts-cfn-cloudfront-identity") | .FunctionName' \
+  | sed -e 's/^"//' -e 's/"$//')
+if [ -n "${LAMBDA}" ]; then
+  aws lambda update-function-code --function-name ${LAMBDA} \
+    --s3-bucket ${BUCKET} --s3-key ${S3PATH}/${PACKAGE}
+fi
+
+#
+# End
